9e1f35bd95f4853d53efb7c2e1322f5f84557372
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-04-11 18:06:53 +0100

    Completed Day 14.

---
+++ AOC2019/Day14/Chemical.cs
@@ -0,0 +1,58 @@
+﻿namespace AOC2019.Day14
+{
+    internal class Chemical
+    {
+        public string Name { get; private set; }
+        public long NumberProduced { get; set; } = 0;
+        public Dictionary<Chemical, long> Parents { get; set; } = new Dictionary<Chemical, long>();
+
+        public Chemical(string name)
+        {
+            Name = name;
+        }
+
+        public void SetNumberProduced(long numberProduced)
+        {
+            NumberProduced = numberProduced;
+        }
+
+        public void AddParent(Chemical parent, long amount)
+        {
+            Parents.Add(parent, amount);
+        }
+        public long CountParents(long numberRequired, Dictionary<Chemical, long> chemicalStockpile)
+        {
+            if (!Parents.Any())
+            {
+                return numberRequired;
+            }
+
+            if (!chemicalStockpile.ContainsKey(this))
+            {
+                chemicalStockpile[this] = 0;
+            }
+
+            if (chemicalStockpile[this] >= numberRequired)
+            {
+                chemicalStockpile[this] -= numberRequired;
+                return 0;
+            }
+
+            numberRequired -= chemicalStockpile[this];
+            chemicalStockpile[this] = 0;
+            long multiplesRequired = 1;
+            if (NumberProduced < numberRequired)
+            {
+                multiplesRequired = numberRequired / NumberProduced;
+                multiplesRequired += numberRequired % NumberProduced == 0 ? 0 : 1;
+            }
+            chemicalStockpile[this] = NumberProduced * multiplesRequired - numberRequired;
+            long sum = 0;
+            foreach (var keyValuePair in Parents)
+            {
+                sum += keyValuePair.Key.CountParents(multiplesRequired * keyValuePair.Value, chemicalStockpile);
+            }
+            return sum;
+        }
+    }
+}
---
+++ AOC2019/Day14/Day14InputHelper.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AOC2019.Day14
+{
+    internal class Day14InputHelper : InputHelper<List<Chemical>>
+    {
+        public Day14InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<Chemical> Parse()
+        {
+            var output = new List<Chemical>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                var chemicalRegex = new Regex(@"\d+\s[A-Z]+");
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    var chemicalMatches = chemicalRegex.Matches(ln);
+
+                    var childChemical = output.FirstOrDefault(x => x.Name == GetChemicalName(chemicalMatches.Last().Value));
+                    if (childChemical == null)
+                    {
+                        childChemical = new Chemical(GetChemicalName(chemicalMatches.Last().Value));
+                        output.Add(childChemical);
+                    }
+                    childChemical.SetNumberProduced(GetChemicalNumber(chemicalMatches.Last().Value));
+
+                    for (var i = 0; i < chemicalMatches.Count - 1; i++)
+                    {
+                        var parentChemical = output.FirstOrDefault(x => x.Name == GetChemicalName(chemicalMatches[i].Value));
+                        if (parentChemical == null)
+                        {
+                            parentChemical = new Chemical(GetChemicalName(chemicalMatches[i].Value));
+                            output.Add(parentChemical);
+                        }
+                        childChemical.AddParent(parentChemical, GetChemicalNumber(chemicalMatches[i].Value));
+                    }
+                }
+            }
+            return output;
+        }
+
+        private string GetChemicalName(string chemicalInput)
+        {
+            var nameRegex = new Regex(@"[A-Z]+");
+            return nameRegex.Match(chemicalInput).Value;
+        }
+
+        private long GetChemicalNumber(string chemicalInput)
+        {
+            var numberRegex = new Regex(@"\d+");
+            return long.Parse(numberRegex.Match(chemicalInput).Value);
+        }
+    }
+}
