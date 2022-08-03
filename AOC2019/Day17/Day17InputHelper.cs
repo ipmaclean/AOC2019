8898dc0426208b78982f81298412fbb5a9fce929
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-08-03 16:44:36 +0100

    Completed Day 17.

---
+++ AOC2019/Day17/Day17InputHelper.cs
@@ -0,0 +1,11 @@
+﻿using AOC2019.IntCode;
+
+namespace AOC2019.Day17
+{
+    internal class Day17InputHelper : IntCodeInputHelper
+    {
+        public Day17InputHelper(string fileName) : base(fileName)
+        {
+        }
+    }
+}
