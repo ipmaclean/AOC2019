9fd9891032a6928d6e96cb52dab146588a73ef96
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-08-05 17:21:05 +0100

    Added a programmatic solution for Day 19 Part 2.

--- AOC2019/Day19/Day19PuzzleManager.cs
+++ AOC2019/Day19/Day19PuzzleManager.cs
@@ -5,6 +5,8 @@ namespace AOC2019.Day19
 {
     public class Day19PuzzleManager : PuzzleManager
     {
+        private const long _partTwoSquareSize = 100;
+
         public Dictionary<long, long> IntCodeProgram { get; private set; }
 
         public Day19PuzzleManager()
@@ -38,23 +40,17 @@ namespace AOC2019.Day19
         {
 
             var solution = 0;
-            var tiles = new Dictionary<(long, long), long>();
+            var tiles = new Dictionary<(long, long), bool>();
             for (var xCoord = 0L; xCoord < 50; xCoord++)
             {
                 for (var yCoord = 0L; yCoord < 50; yCoord++)
                 {
-                    var codeInput = new Dictionary<long, long>(IntCodeProgram);
-                    var inputs = new Queue<long>();
-                    inputs.Enqueue(xCoord);
-                    inputs.Enqueue(yCoord);
-                    var intCodeComputer = new IntCodeComputer(codeInput, inputs);
-                    await intCodeComputer.ProcessAsync();
-                    var output = intCodeComputer.Outputs.Dequeue();
-                    if (output == 1)
+                    var coordinateIsInBeam = await CoordinateIsInTractorBeam(xCoord, yCoord);
+                    if (coordinateIsInBeam)
                     {
                         solution++;
                     }
-                    tiles.Add((xCoord, yCoord), output);
+                    tiles.Add((xCoord, yCoord), coordinateIsInBeam);
                 }
             }
             if (printTractorBeam)
@@ -64,7 +60,19 @@ namespace AOC2019.Day19
             return solution;
         }
 
-        private void PrintResult(Dictionary<(long, long), long> tiles)
+        private async Task<bool> CoordinateIsInTractorBeam(long xCoord, long yCoord)
+        {
+            var codeInput = new Dictionary<long, long>(IntCodeProgram);
+            var inputs = new Queue<long>();
+            inputs.Enqueue(xCoord);
+            inputs.Enqueue(yCoord);
+            var intCodeComputer = new IntCodeComputer(codeInput, inputs);
+            await intCodeComputer.ProcessAsync();
+            var output = intCodeComputer.Outputs.Dequeue();
+            return output == 1;
+        }
+
+        private void PrintResult(Dictionary<(long, long), bool> tiles)
         {
             var highestX = tiles.Max(x => x.Key.Item1);
             var highestY = tiles.Max(x => x.Key.Item2);
@@ -73,7 +81,7 @@ namespace AOC2019.Day19
 
             foreach (var tile in tiles)
             {
-                canvas[tile.Key.Item1, tile.Key.Item2] = tile.Value == 1 ? '#' : '.';
+                canvas[tile.Key.Item1, tile.Key.Item2] = tile.Value ? '#' : '.';
             }
 
             var sb = new StringBuilder();
@@ -92,7 +100,7 @@ namespace AOC2019.Day19
             Console.Write(sb.ToString());
         }
 
-        public override Task SolvePartTwo()
+        public override async Task SolvePartTwo()
         {
             // For part two, I didn't solve programmatically. I printed out a large square (I chose 1250x1250 but it doesn't need to be quite that big - 918x1122 would have been just enough for me) and then examined it in Excel.
             // I noticed that the x-coordinate of the top left of the first nxn square that fits in the beam is the first column that has a height of 2n-1.
@@ -100,8 +108,34 @@ namespace AOC2019.Day19
 
             // I may write a program at some point using these insights but it didn't seem worth doing for the sake of this puzzle, especially with part one being so easy.
 
-            Console.WriteLine($"The solution to part one is '9181022'.");
-            return Task.CompletedTask;
+            //Console.WriteLine($"The solution to part two is '9181022'.");
+
+            // EDIT: Ok, I solved it programmatically (after browsing the AoC subreddit for inspiration/straight up telling me the answer).
+
+            var solution = await SolvePartTwoPrivateAsync();
+            Console.WriteLine($"The solution to part two is '{solution}'.");
+        }
+
+        public async Task<long> SolvePartTwoPrivateAsync()
+        {
+            var xCoord = 0L;
+            var yCoord = _partTwoSquareSize - 1;
+
+            while (true)
+            {
+                if (await CoordinateIsInTractorBeam(xCoord, yCoord))
+                {
+                    if (await CoordinateIsInTractorBeam(xCoord + _partTwoSquareSize - 1, yCoord - (_partTwoSquareSize - 1)))
+                    {
+                        return xCoord * 10_000 + yCoord - (_partTwoSquareSize - 1);
+                    }
+                    yCoord++;
+                }
+                else
+                {
+                    xCoord++;
+                }
+            }
         }
     }
 }
