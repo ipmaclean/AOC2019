908033480f5ddc0668a08fb0a0d3a3f1e54a5ce0
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-08-12 12:09:30 +0100

    Solved Part Two.

--- AOC2019/Day24/Day24PuzzleManager.cs
+++ AOC2019/Day24/Day24PuzzleManager.cs
@@ -7,8 +7,6 @@ namespace AOC2019.Day24
 
         public List<Tile> Tiles { get; set; }
 
-        protected override string INPUT_FILE_NAME { get; set; } = "test1.txt";
-
         public Day24PuzzleManager()
         {
             var inputHelper = new Day24InputHelper(INPUT_FILE_NAME);
@@ -17,7 +15,9 @@ namespace AOC2019.Day24
 
         public override Task SolveBothParts()
         {
-            throw new NotImplementedException();
+            SolvePartOne();
+            SolvePartTwo();
+            return Task.CompletedTask;
         }
 
         public override Task SolvePartOne()
@@ -36,34 +36,6 @@ namespace AOC2019.Day24
                 biodiversityRatings.Add(biodiversityRating);
                 tiles = RunGameOfLifePartOne(tiles);
             }
-        }
-
-        private void Print(List<Tile> tiles)
-        {
-            Console.Clear();
-            var xCoordCounter = 0;
-            var sb = new StringBuilder();
-            foreach (var tile in tiles.OrderBy(x => x.Coordinates.Y).ThenBy(x => x.Coordinates.X))
-            {
-                if (xCoordCounter++ % 5 == 0)
-                {
-                    sb.Append(Environment.NewLine);
-                }
-                if (tile.IsBug)
-                {
-                    sb.Append("#");
-                }
-                else
-                {
-                    sb.Append(".");
-                }
-            }
-            Console.Write(sb.ToString());
-        }
-
-        public override Task SolvePartTwo()
-        {
-            throw new NotImplementedException();
         }
 
         private List<Tile> RunGameOfLifePartOne(List<Tile> oldTiles)
@@ -128,6 +100,228 @@ namespace AOC2019.Day24
                     continue;
                 }
                 if (tiles.First(x => x.Coordinates == coordToCheck).IsBug)
+                {
+                    bugCount++;
+                }
+            }
+            return bugCount;
+        }
+
+        public override Task SolvePartTwo()
+        {
+            var tiles = DeepCopyTiles(Tiles);
+
+            for (var i = 0; i < 200; i++)
+            {
+                tiles = RunGameOfLifePartTwo(tiles);
+            }
+            Console.WriteLine($"The solution to part two is '{tiles.Count(x => x.IsBug)}'.");
+            return Task.CompletedTask;
+        }
+
+        private List<Tile> RunGameOfLifePartTwo(List<Tile> oldTiles)
+        {
+            var newTiles = new List<Tile>();
+            var maxLevel = oldTiles.Max(x => x.Level);
+            var minLevel = oldTiles.Min(x => x.Level);
+            var outsideTiles = new (int, int)[]
+            {
+                (0, 0),
+                (1, 0),
+                (2, 0),
+                (3, 0),
+                (4, 0),
+                (4, 1),
+                (4, 2),
+                (4, 3),
+                (4, 4),
+                (3, 4),
+                (2, 4),
+                (1, 4),
+                (0, 4),
+                (0, 3),
+                (0, 2),
+                (0, 1)
+            };
+            if (oldTiles.Any(x => x.Level == minLevel && x.IsBug && outsideTiles.Contains(x.Coordinates)))
+            {
+                AddEmptyLevel(oldTiles, minLevel - 1);
+            }
+
+            var insideTiles = new (int, int)[]
+            {
+                (1, 1),
+                (2, 1),
+                (3, 1),
+                (3, 2),
+                (3, 3),
+                (2, 3),
+                (1, 3),
+                (1, 2)
+            };
+            if (oldTiles.Any(x => x.Level == maxLevel && x.IsBug && insideTiles.Contains(x.Coordinates)))
+            {
+                AddEmptyLevel(oldTiles, maxLevel + 1);
+            }
+
+            foreach (var tile in oldTiles)
+            {
+                if (tile.Coordinates == (2, 2))
+                {
+                    continue;
+                }
+                var bugCount = CountAdjacentBugsPartTwo(tile, oldTiles);
+                if (tile.IsBug && bugCount == 1)
+                {
+                    newTiles.Add(new Tile(tile.Coordinates, true, tile.Level));
+                }
+                else if (!tile.IsBug && (bugCount == 1 || bugCount == 2))
+                {
+                    newTiles.Add(new Tile(tile.Coordinates, true, tile.Level));
+                }
+                else
+                {
+                    newTiles.Add(new Tile(tile.Coordinates, false, tile.Level));
+                }
+            }
+
+            return newTiles;
+        }
+
+        private void AddEmptyLevel(List<Tile> tiles, int level)
+        {
+            for (var y = 0; y < 5; y++)
+            {
+                for (var x = 0; x < 5; x++)
+                {
+                    tiles.Add(new Tile((x, y), false, level));
+                }
+            }
+        }
+
+        private int CountAdjacentBugsPartTwo(Tile tile, List<Tile> tiles)
+        {
+            var maxX = 4;
+            var maxY = 4;
+
+            var adjacentCoords = new (int X, int Y)[]
+            {
+                (0, 1),
+                (0, -1),
+                (1, 0),
+                (-1, 0)
+            };
+
+            var bugCount = 0;
+            foreach (var adjacentCoord in adjacentCoords)
+            {
+                (int X, int Y) coordToCheck = (tile.Coordinates.X + adjacentCoord.X, tile.Coordinates.Y + adjacentCoord.Y);
+
+                if (coordToCheck.X < 0)
+                {
+                    if (tiles.Any(x => x.Level == tile.Level - 1))
+                    {
+                        bugCount = tiles.First(x => x.Coordinates == (1, 2) && x.Level == tile.Level - 1).IsBug ? bugCount + 1 : bugCount;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (coordToCheck.X > maxX)
+                {
+                    if (tiles.Any(x => x.Level == tile.Level - 1))
+                    {
+                        bugCount = tiles.First(x => x.Coordinates == (3, 2) && x.Level == tile.Level - 1).IsBug ? bugCount + 1 : bugCount;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (coordToCheck.Y < 0)
+                {
+                    if (tiles.Any(x => x.Level == tile.Level - 1))
+                    {
+                        bugCount = tiles.First(x => x.Coordinates == (2, 1) && x.Level == tile.Level - 1).IsBug ? bugCount + 1 : bugCount;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (coordToCheck.Y > maxY)
+                {
+                    if (tiles.Any(x => x.Level == tile.Level - 1))
+                    {
+                        bugCount = tiles.First(x => x.Coordinates == (2, 3) && x.Level == tile.Level - 1).IsBug ? bugCount + 1 : bugCount;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (coordToCheck == (2, 2))
+                {
+                    if (tiles.Any(x => x.Level == tile.Level + 1))
+                    {
+                        (int, int)[] tilesToCheck;
+                        if (tile.Coordinates == (1, 2))
+                        {
+                            tilesToCheck = new (int, int)[]
+                            {
+                                (0, 0),
+                                (0, 1),
+                                (0, 2),
+                                (0, 3),
+                                (0, 4),
+                            };
+                        }
+                        else if (tile.Coordinates == (3, 2))
+                        {
+                            tilesToCheck = new (int, int)[]
+                            {
+                                (4, 0),
+                                (4, 1),
+                                (4, 2),
+                                (4, 3),
+                                (4, 4),
+                            };
+                        }
+                        else if (tile.Coordinates == (2, 1))
+                        {
+                            tilesToCheck = new (int, int)[]
+                            {
+                                (0, 0),
+                                (1, 0),
+                                (2, 0),
+                                (3, 0),
+                                (4, 0),
+                            };
+                        }
+                        else if (tile.Coordinates == (2, 3))
+                        {
+                            tilesToCheck = new (int, int)[]
+                            {
+                                (0, 4),
+                                (1, 4),
+                                (2, 4),
+                                (3, 4),
+                                (4, 4),
+                            };
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Impossible tile.");
+                        }
+                        bugCount += tiles.Count(x => x.Level == tile.Level + 1 && tilesToCheck.Contains(x.Coordinates) && x.IsBug);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (tiles.First(x => x.Coordinates == coordToCheck && x.Level == tile.Level).IsBug)
                 {
                     bugCount++;
                 }
